2296f876e0a50da93a909003b135f4a87056f65a
jogosdiariosdefutebol-stack <jogosdiariosdefutebol@gmail.com>
2026-06-04 12:58:50 -0300

    Update next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-};
-
-export default nextConfig;
+module.exports = nextConfig;
