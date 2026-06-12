f666f9a7fcc43d4209def5acea7ab13d23577f60
jogosdiariosdefutebol-stack <jogosdiariosdefutebol@gmail.com>
2026-06-12 07:32:20 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "FutJogos — Jogos diários de futebol",
@@ -21,6 +22,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-31EWWMH1FP"
+      strategy="afterInteractive"
+          />
+  <Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-31EWWMH1FP');
+  `}
+</Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Nunito:wght@400;600;700;800;900&display=swap" rel="stylesheet" />
